28c0d84d8ae4c63c764b2fd9cdc539abb1eb7b01
Fabian Fernandez <fabianfernandez879@gmail.com>
2023-09-06 23:26:10 -0300

    actualice js e index

--- js/main.js
+++ js/main.js
@@ -24,87 +24,98 @@ let productos = [
     {id: 7, nombre: "Pulseras", categoria: "accesorios", precio: 800, stock: 18},
     {id: 8, nombre: "Cosplays", categoria: "disfraces", precio: 6500, stock: 16}
 ]
-let buscador = prompt(`                                              ¿Qué buscas?
-Funko pops
-Figuras de acción
-Juguetes
-Mangas
-CDs
-Camisetas
-Pulseras
-Cosplays`)
-if ((buscador === "") || (buscador === " ")){
-    alert("No hay resultados")
-}if(buscador === "funko pops"){
-    let pregunta = confirm(`¿Quieres Funko pops?`)
-    if(pregunta === true){
-        let buscaProductos = productos.filter(elem => elem.nombre === "Funko pops")
-        console.log(buscaProductos)
-        alert("Abre la consola X'D")
-    }else{
-        alert("Ah. Ok... :'v")
-    }
-}if(buscador === "figuras de accion"){
-    let pregunta = confirm(`¿Quieres Figuras de acción?`)
-    if(pregunta === true){
-        let buscaProductos = productos.filter(elem => elem.nombre === "Figuras de acción")
-        console.log(buscaProductos)
-        alert("Abre la consola X'D")
-    }else{
-        alert("Ah. Ok... :'v")
-    }
-}if(buscador === "juguetes"){
-    let pregunta = confirm(`¿Quieres Juguetes?`)
-    if(pregunta === true){
-        let buscaProductos = productos.filter(elem => elem.nombre === "Juguetes")
-        console.log(buscaProductos)
-        alert("Abre la consola X'D")
-    }else{
-        alert("Ah. Ok... :'v")
-    }
-}if(buscador === "mangas"){
-    let pregunta = confirm(`¿Quieres Mangas?`)
-    if(pregunta === true){
-        let buscaProductos = productos.filter(elem => elem.nombre === "Mangas")
-        console.log(buscaProductos)
-        alert("Abre la consola X'D")
-    }else{
-        alert("Ah. Ok... :'v")
-    }
-}if(buscador === "cds"){
-    let pregunta = confirm(`¿Quieres CDs?`)
-    if(pregunta === true){
-        let buscaProductos = productos.filter(elem => elem.nombre === "CDs")
-        console.log(buscaProductos)
-        alert("Abre la consola X'D")
-    }else{
-        alert("Ah. Ok... :'v")
-    }
-}if(buscador === "camisetas"){
-    let pregunta = confirm(`¿Quieres Camisetas?`)
-    if(pregunta === true){
-        let buscaProductos = productos.filter(elem => elem.nombre === "Camisetas")
-        console.log(buscaProductos)
-        alert("Abre la consola X'D")
-    }else{
-        alert("Ah. Ok... :'v")
-    }
-}if(buscador === "pulseras"){
-    let pregunta = confirm(`¿Quieres Pulseras?`)
-    if(pregunta === true){
-        let buscaProductos = productos.filter(elem => elem.nombre === "Pulseras")
-        console.log(buscaProductos)
-        alert("Abre la consola X'D")
-    }else{
-        alert("Ah. Ok... :'v")
-    }
-}if(buscador === "cosplays"){
-    let pregunta = confirm(`¿Quieres Cosplays?`)
-    if(pregunta === true){
-        let buscaProductos = productos.filter(elem => elem.nombre === "Cosplays")
-        console.log(buscaProductos)
-        alert("Abre la consola X'D")
-    }else{
-        alert("Ah. Ok... :'v")
+let artiContenedor = document.getElementById("articulos")
+productos.forEach(producto => {
+    let tarjetaDeArticulo = document.createElement("article")
+    tarjetaDeArticulo.innerHTML = `<h3>${producto.nombre}</h3>`
+    artiContenedor.append(tarjetaDeArticulo)
+})
+
+let elegir = document.getElementById("elige")
+elegir.onclick = funcionCartel
+function funcionCartel(){
+    let buscador = prompt(`                                              ¿Qué buscas?
+    Funko pops
+    Figuras de acción
+    Juguetes
+    Mangas
+    CDs
+    Camisetas
+    Pulseras
+    Cosplays`)
+    if ((buscador === "") || (buscador === " ")){
+        alert("No hay resultados")
+    }if(buscador === "funko pops"){
+        let pregunta = confirm(`¿Quieres Funko pops?`)
+        if(pregunta === true){
+            let buscaProductos = productos.filter(elem => elem.nombre === "Funko pops")
+            console.log(buscaProductos)
+            alert("Abre la consola X'D")
+        }else{
+            alert("Ah. Ok... :'v")
+        }
+    }if(buscador === "figuras de accion"){
+        let pregunta = confirm(`¿Quieres Figuras de acción?`)
+        if(pregunta === true){
+            let buscaProductos = productos.filter(elem => elem.nombre === "Figuras de acción")
+            console.log(buscaProductos)
+            alert("Abre la consola X'D")
+        }else{
+            alert("Ah. Ok... :'v")
+        }
+    }if(buscador === "juguetes"){
+        let pregunta = confirm(`¿Quieres Juguetes?`)
+        if(pregunta === true){
+            let buscaProductos = productos.filter(elem => elem.nombre === "Juguetes")
+            console.log(buscaProductos)
+            alert("Abre la consola X'D")
+        }else{
+            alert("Ah. Ok... :'v")
+        }
+    }if(buscador === "mangas"){
+        let pregunta = confirm(`¿Quieres Mangas?`)
+        if(pregunta === true){
+            let buscaProductos = productos.filter(elem => elem.nombre === "Mangas")
+            console.log(buscaProductos)
+            alert("Abre la consola X'D")
+        }else{
+            alert("Ah. Ok... :'v")
+        }
+    }if(buscador === "cds"){
+        let pregunta = confirm(`¿Quieres CDs?`)
+        if(pregunta === true){
+            let buscaProductos = productos.filter(elem => elem.nombre === "CDs")
+            console.log(buscaProductos)
+            alert("Abre la consola X'D")
+        }else{
+            alert("Ah. Ok... :'v")
+        }
+    }if(buscador === "camisetas"){
+        let pregunta = confirm(`¿Quieres Camisetas?`)
+        if(pregunta === true){
+            let buscaProductos = productos.filter(elem => elem.nombre === "Camisetas")
+            console.log(buscaProductos)
+            alert("Abre la consola X'D")
+        }else{
+            alert("Ah. Ok... :'v")
+        }
+    }if(buscador === "pulseras"){
+        let pregunta = confirm(`¿Quieres Pulseras?`)
+        if(pregunta === true){
+            let buscaProductos = productos.filter(elem => elem.nombre === "Pulseras")
+            console.log(buscaProductos)
+            alert("Abre la consola X'D")
+        }else{
+            alert("Ah. Ok... :'v")
+        }
+    }if(buscador === "cosplays"){
+        let pregunta = confirm(`¿Quieres Cosplays?`)
+        if(pregunta === true){
+            let buscaProductos = productos.filter(elem => elem.nombre === "Cosplays")
+            console.log(buscaProductos)
+            alert("Abre la consola X'D")
+        }else{
+            alert("Ah. Ok... :'v")
+        }
     }
 }
